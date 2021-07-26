1cae3c60c6b84f2e5dfe47bb76660c3ffecdeab1
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-07-26 16:30:04 +0900

    0726
    Add models

---
+++ iOS Square Detection App/ARKitProject/DistanceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DistanceViewController.swift
+//  ARKitProject
+//
+//  Created by JungJiyoung on 2021/07/26.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+// 거리 재는 ARRuler 구현 예정
+
+import ARKit
+import UIKit
+import Photos
+
+class DistanceViewController : UIViewController {
+    
+}
