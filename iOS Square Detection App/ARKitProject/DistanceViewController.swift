306eb2c4541c347b00ed7f0ecacb02ec2e56d441
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-07-26 09:07:19 +0900

    0726
    Add ruler button

---
+++ iOS Square Detection App/ARKitProject/DistanceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DistanceViewController.swift
+//  ARKitProject
+//
+//  Created by JungJiyoung on 2021/07/26.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import Photos
+
+class DistanceViewController: UIViewController {
+    @IBOutlet var sceneView: ARSCNView!
+}
