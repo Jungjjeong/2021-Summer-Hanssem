84ec9f57eda86a520db000a0c50e29d249434bb6
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-08-03 16:24:39 +0900

    feat : Size Measurement code

--- iOS Square Detection App/ARKitProject/SizeMeasurementView.swift
+++ iOS Square Detection App/ARKitProject/SizeMeasurementView.swift
@@ -11,38 +11,116 @@ import ARKit
 import UIKit
 
 
-class SizeMeasurementView : UIViewController {
+class SizeMeasurementView : UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
     
+    var doteNodes = [SCNNode]()
+    var textNode = SCNNode()
     
-    override func viewDidLoad() { // view initialized
+    override func viewDidLoad() {
         super.viewDidLoad()
-
-        Setting.registerDefaults()
         
-        sceneView.automaticallyUpdatesLighting = true
+        sceneView.delegate = self
         sceneView.autoenablesDefaultLighting = true
-    }
-
-    override func viewDidAppear(_ animated: Bool) { // view 보여진 후, animation appear
-        super.viewDidAppear(animated)
-
-        UIApplication.shared.isIdleTimerDisabled = true // 일정 시간 지나면 꺼지는 타이머가 설정 되지 않았는가 -> true
-        // 계속해서 contents를 표시해야 하는 mapping app 같은 경우, 유후 카메라를 끈다.
-        restartPlaneDetection() // 뷰가 나타날때마다 plane detection을 수행한다.
-    }
-
-    override func viewWillDisappear(_ animated: Bool) { // view 사라지기 전
-        super.viewWillDisappear(animated)
-        let configuration = ARWorldTrackingConfiguration()
-        configuration.isLightEstimationEnabled = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
-    func restartPlaneDetection() {
-        // configure session
-        if let worldSessionConfig = sessionConfig as? ARWorldTrackingConfiguration { // ARWorldTrackingConfiguration 의 sessionConfig로 다운캐스팅
-            worldSessionConfig.planeDetection = .horizontal // 수평 planeDetection
-            session.run(worldSessionConfig, options: [.resetTracking, .removeExistingAnchors]) // 전에 존재하던 anchor 제거, tracking 초기화
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARWorldTrackingConfiguration()
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if doteNodes.count >= 2 {
+            for dot in doteNodes{
+                dot.removeFromParentNode()
+            }
+            doteNodes = [SCNNode]()
         }
+        
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: sceneView)
+            
+            let results = sceneView.raycastQuery(from: touchLocation, allowing: ARRaycastQuery.Target.estimatedPlane, alignment: .any)
+            
+            if let hitRes = results {
+                let rayCast = sceneView.session.raycast(hitRes)
+                
+                guard let ray = rayCast.first else { return }
+                addDot(at : ray)
+            }
+        }
+    }
+    
+    
+    func addDot(at hitResult: ARRaycastResult) {
+        let sphereScene = SCNSphere(radius: 0.01)
+        
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.white
+        sphereScene.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(
+            hitResult.worldTransform.columns.3.x,
+            hitResult.worldTransform.columns.3.y + sphereScene.boundingSphere.radius,
+            hitResult.worldTransform.columns.3.z
+        )
+        
+        node.geometry = sphereScene
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        doteNodes.append(node)
+        
+        if doteNodes.count >= 2{
+            calculate()
+        }
+    }
+    
+    
+    func calculate() {
+        let start = doteNodes[0]
+        let end = doteNodes[1]
+        
+        print(start.position)
+        print(end.position)
+        
+        let distance = sqrt(pow(start.position.x-end.position.x, 2) +
+                                pow(start.position.y-end.position.y, 2) +
+                                pow(start.position.z-end.position.z, 2))
+        
+        
+        updateText(text: "\(abs(distance))", atPosition: start.position)
+    }
+    
+    
+    func updateText(text: String, atPosition position: SCNVector3){
+        textNode.removeFromParentNode()
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.white
+        
+        textNode = SCNNode(geometry: textGeometry)
+        
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        
+        textNode.scale = SCNVector3(0.005, 0.005, 0.005)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
     }
 }
