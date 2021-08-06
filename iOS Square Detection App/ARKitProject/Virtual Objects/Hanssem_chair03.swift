c2f94a41ee01fe81079e225ffe1f1913d7f8e3f4
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-08-06 16:57:42 +0900

    feat : add Hanssem chair fbx file

---
+++ iOS Square Detection App/ARKitProject/Virtual Objects/Hanssem_chair03.swift
@@ -0,0 +1,21 @@
+//
+//  Hanssem_chair03.swift
+//  ARKitProject
+//
+//  Created by JungJiyoung on 2021/08/06.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import Foundation
+
+
+class Hanssem_chair03: VirtualObject{
+
+    override init() {
+        super.init(modelName: "Hanssem_chair03", fileExtension: "usdz", thumbImageFilename: "vase", title: "Hanssem_chair03")
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
