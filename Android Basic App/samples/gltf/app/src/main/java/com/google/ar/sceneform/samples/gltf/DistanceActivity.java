306bc24eeec0640a9617fc9e5797d03a384bbdf7
Jungjjeong <72294509+Jungjjeong@users.noreply.github.com>
2021-08-13 16:53:13 +0900

    feat : Add android app

---
+++ Android Basic App/samples/gltf/app/src/main/java/com/google/ar/sceneform/samples/gltf/DistanceActivity.java
@@ -0,0 +1,177 @@
+// 거리측정 페이지
+
+package com.google.ar.sceneform.samples.gltf;
+
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.assist.AssistStructure;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.camera2.params.SessionConfiguration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.Frame;
+import com.google.ar.core.Pose;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.Color;
+import com.google.ar.sceneform.rendering.MaterialFactory;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.ShapeFactory;
+import com.google.ar.sceneform.rendering.Texture;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.util.Objects;
+
+public class DistanceActivity extends AppCompatActivity implements Scene.OnUpdateListener {
+
+    private static final double MIN_OPENGL_VERSION = 3.0;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private ArFragment arFragment;
+    private Anchor[] currentAnchor = new Anchor[2]; // anchor array
+    private Vector3[] currentVector = new Vector3[2];
+    private AnchorNode[] currentAnchorNode = new AnchorNode[2];
+    // anchorNode 배열 선언 (Node : 하나의 object가 차지하는 영역, 즉 anchorNode는 하나의 anchor object가 차지하는 영역)
+    private TextView tvDistance; // Distance 보여주는 textview
+    ModelRenderable cubeRenderable; // sceneform renderable
+    public static int cnt = 0; // count anchor
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!checkIsSupportedDeviceOrFinish(this)) { // requirements check
+            Toast.makeText(getApplicationContext(), "Device not supported", Toast.LENGTH_LONG).show();
+        }
+
+        setContentView(R.layout.activity_distance); // activity_distance.xml
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment); // ux_fragment.xml -> fragment manager -> arfragment
+        tvDistance = findViewById(R.id.tvDistance); // Distance Textview
+
+
+        initModel();
+
+        arFragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> { // Plane의 white dot tap하면 function 실행
+            if (cubeRenderable == null)
+                return;
+
+
+            // Creating Anchor (Anchor : fixed location and orientation in real world -> rendering 3D model in Anchor)
+            Anchor anchor = hitResult.createAnchor();
+            Vector3 vec = new Vector3(hitResult.getHitPose().qx(), hitResult.getHitPose().qy(), hitResult.getHitPose().qz());
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene()); // anchorNode Parrent -> Scene (plane)
+
+            clearAnchor(); // function -> cnt == 2일경우 initialize
+
+            currentAnchor[cnt] = anchor;
+            currentVector[cnt] = vec;
+            currentAnchorNode[cnt] = anchorNode; // anchor 생성마다 array 할당
+
+            cnt++; // anchor count ++
+
+            TransformableNode node = new TransformableNode(arFragment.getTransformationSystem()); // 확대, 축소, 변형 Node
+            node.setRenderable(cubeRenderable); // set renderable
+            node.setParent(anchorNode); // node Parrent -> anchornode
+            arFragment.getArSceneView().getScene().addOnUpdateListener(this);
+            arFragment.getArSceneView().getScene().addChild(anchorNode);
+            node.select();
+        });
+
+    }
+
+    
+
+    public boolean checkIsSupportedDeviceOrFinish(final Activity activity) { // requirements checked
+
+        String openGlVersionString =
+                ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE)))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG)
+                    .show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+
+    private void initModel() {
+        MaterialFactory.makeTransparentWithColor(this, new Color(android.graphics.Color.RED))
+                .thenAccept(
+                        material -> {
+                            Vector3 vector3 = new Vector3(0.02f, 0.02f, 0.02f);
+                            cubeRenderable = ShapeFactory.makeCube(vector3, Vector3.zero(), material);
+                            cubeRenderable.setShadowCaster(false);
+                            cubeRenderable.setShadowReceiver(false);
+                        });
+    }
+
+    private void clearAnchor() { // clear Anchor
+        if(cnt == 2){ // tap hitresult가 2개 초과일 수 없음
+            for(int i=0; i<2; i++){
+                currentAnchor[i] = null;
+                currentVector[i] = null;
+                if (currentAnchorNode[i] != null) {
+                    arFragment.getArSceneView().getScene().removeChild(currentAnchorNode[i]);
+                    currentAnchorNode[i].getAnchor().detach();
+                    currentAnchorNode[i].setParent(null);
+                    currentAnchorNode = null;
+                }
+            }
+            cnt = 0; // cnt, currentAnchor, currentAnchorNode initialize
+        }
+    }
+
+
+    @Override
+    public void onUpdate(FrameTime frameTime) { // scene이 update 되기 전 frame당 한번씩 호출
+        Frame frame = arFragment.getArSceneView().getArFrame();
+
+        Intent intent = getIntent();
+        int length = (int) intent.getSerializableExtra("length"); //  get length
+
+
+        if (currentAnchorNode != null && cnt == 2) {
+            Pose objectPose = currentAnchor[0].getPose();
+            Pose newPose = currentAnchor[1].getPose(); // get each 2 anchors pose
+
+            float dx = objectPose.tx() - newPose.tx();
+            float dy = objectPose.ty() - newPose.ty();
+            float dz = objectPose.tz() - newPose.tz(); // current x,y,z pose distance
+
+            ///Compute distance.
+            float distanceMeters = (float) Math.sqrt(dx * dx + dy * dy + dz * dz); // distance
+
+
+            tvDistance.setText("측정된 길이: " + (float)Math.round(distanceMeters*100) + " cm");
+
+            Intent placeIntent = new Intent(DistanceActivity.this, PlaceActivity.class); // Intent -> PlaceActivity
+            placeIntent.putExtra("distance", (float)Math.round(distanceMeters*100)); // distance
+            placeIntent.putExtra("length", length); // furniture length
+
+            startActivity(placeIntent);
+
+            finish();
+
+        }
+    }
+
+
+}
---
+++ Android Basic App/samples/gltf/app/src/main/java/com/google/ar/sceneform/samples/gltf/ListARActivity.java
@@ -0,0 +1,80 @@
+// AR furniture List
+
+package com.google.ar.sceneform.samples.gltf;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListARActivity extends AppCompatActivity {
+
+    ArrayList<SampleData> dataList; // 실제로 list 내 데이터를 저장하게 될 type이 SampleData인 ArrayList 객체 생성
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_aractivity);// 레이아웃 정의한 레이아웃 리소스를 사용하여 현재 액티비티의 화면 구성
+
+        this.InitializeData(); // Initialize
+
+        ListView listView = (ListView) findViewById(R.id.listview); // 레이아웃 파일에 정의된 listview
+        final ListAdapter listadapter = new ListAdapter(this, dataList); // ListAdapter extends BasicAdapter -> ListAdapter.java
+
+        listView.setAdapter(listadapter); // listview 객체에 Listadapter 객체 연결
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() { // Click Item
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long l) {
+                System.out.println("--------------------------------------페이지 실행--------------------------------------------");
+                Intent intent = new Intent(ListARActivity.this, GltfActivity.class); // ListARActivity -> GltfActivity data 이동
+                intent.putExtra("key", listadapter.getItem(position).getKey());
+                intent.putExtra("size", listadapter.getItem(position).getSize()); // 상품의 key, size 전달
+
+                startActivity(intent);
+
+//                Toast.makeText(getApplicationContext(), listadapter.getItem(position).getFullName(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void InitializeData(){
+        dataList = new ArrayList<SampleData>();
+
+        dataList.add(new SampleData(R.drawable.i668317, 0, "한샘 하이 엠마 천연가죽 3인용 소파", 211));
+        dataList.add(new SampleData(R.drawable.i668318, 1, "한샘 하이 엠마 천연가죽 4인용 소파", 301));
+        dataList.add(new SampleData(R.drawable.i681946, 2, "한샘 하이 모먼트 헤드무빙 천연가죽 소파 4인용", 285));
+        dataList.add(new SampleData(R.drawable.i681947, 3, "한샘 하이 모먼트 헤드무빙 천연가죽 소파 3인용", 210));
+        dataList.add(new SampleData(R.drawable.i737686, 4, "한샘 하이 브리오 이태리 천연가죽 4인용 소파", 285));
+        dataList.add(new SampleData(R.drawable.i737687, 5, "한샘 하이 브리오 이태리 천연가죽 3.5인용 소파", 240));
+        dataList.add(new SampleData(R.drawable.i746525, 6, "한샘 프라임 노블 천연면피가죽 전동 리클라이너 소파 4인용", 263));
+        dataList.add(new SampleData(R.drawable.i746526, 7, "한샘 프라임 노블 천연면피가죽 전동 리클라이너 소파 3인용", 192));
+        dataList.add(new SampleData(R.drawable.i746540, 8, "한샘 프라임 리츠 천연면피가죽 전동 리클라이너 소파 4인용", 263));
+        dataList.add(new SampleData(R.drawable.i746541, 9, "한샘 프라임 리츠 천연면피가죽 전동 리클라이너 소파 3인용", 192));
+        dataList.add(new SampleData(R.drawable.i772973, 10, "한샘 클로즈 침대 SS 슈퍼싱글 코튼그레이", 116));
+        dataList.add(new SampleData(R.drawable.i777039, 11, "한샘 밀로 패브릭소파 3인용", 200));
+        dataList.add(new SampleData(R.drawable.i777040, 12, "한샘 밀로 패브릭소파 4인용", 280));
+        dataList.add(new SampleData(R.drawable.i786840, 13, "한샘 모아 모듈형 패브릭소파 3인용", 252));
+        dataList.add(new SampleData(R.drawable.i786841, 14, "한샘 모아 모듈형 패브릭소파 3인 카우치", 252));
+        dataList.add(new SampleData(R.drawable.i786842, 15, "한샘 모아 모듈형 패브릭소파 4인 오픈형", 280));
+        dataList.add(new SampleData(R.drawable.i787819, 16, "한샘 듀이 아르떼 패브릭소파 4인 카우치", 301));
+        dataList.add(new SampleData(R.drawable.i787823, 17, "한샘 듀이 아르떼 패브릭소파 4인용", 301));
+        dataList.add(new SampleData(R.drawable.i796379, 18, "한샘 엠마 테일러 천연면피가죽 4인용 소파", 278));
+        dataList.add(new SampleData(R.drawable.i796416, 19, "한샘 엠마 테일러 천연면피가죽 3인용 소파", 185));
+        dataList.add(new SampleData(R.drawable.i799215, 20, "한샘 엠마 컴포트 천연면피가죽 3인용 소파", 200));
+        dataList.add(new SampleData(R.drawable.i799220, 21, "한샘 엠마 컴포트 천연면피가죽 4인용 소파", 268));
+    }
+}
