c1263db8706aabb51599eb437cf5c98132c49da8
jungYam <sky990106@dgu.ac.kr>
2021-10-13 15:35:03 +0900

    feat : Update Design

--- iOS Square Detection App/ARKitProject/UI Elements/UIRoundPrimaryButton.swift
+++ iOS Square Detection App/ARKitProject/UI Elements/UIRoundPrimaryButton.swift
@@ -14,7 +14,23 @@ class UIRoundPrimaryButton: UIButton{
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
         self.layer.cornerRadius = 10.0;
-        self.backgroundColor = UIColor(red: 0.5529, green: 0, blue: 0.6392, alpha: 1.0)
-        self.tintColor = UIColor.white
+        self.backgroundColor = UIColor(red: 0.917, green: 0.715, blue: 0, alpha: 1)
+        
+
+//        let layer1 = CAGradientLayer()
+//        layer1.colors = [
+//          UIColor(red: 1, green: 1, blue: 1, alpha: 0.65).cgColor,
+//          UIColor(red: 1, green: 0.262, blue: 0.262, alpha: 0.65).cgColor
+//        ]
+//
+//        layer1.locations = [0, 0.99]
+//        layer1.startPoint = CGPoint(x: 0.25, y: 0.5)
+//        layer1.endPoint = CGPoint(x: 0.75, y: 0.5)
+//        layer1.transform = CATransform3DMakeAffineTransform(CGAffineTransform(a: 1, b: 0, c: 0, d: 27.85, tx: 0, ty: -13.43))
+//        layer1.bounds = self.bounds.insetBy(dx: -0.5*self.bounds.size.width, dy: -0.5*self.bounds.size.height)
+//        layer1.position = self.center
+//        self.layer.addSublayer(layer1)
+
+
     }
 }
